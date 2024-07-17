075d72ee217658d890809649fd174f5e02d7ffb7
Gal Shimer <galshimer2@gmail.com>
2024-07-17 16:08:41 +0300

    css connection

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { RootCmp } from './RootCmp'
 import './index.css'
 
+import './assets/styles/main.scss'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RootCmp />
