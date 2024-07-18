4e3cd7653654d0d396eb27fc5004c97b4eb1b179
Denis Libin <dnslibin4@gmail.com>
2024-07-18 10:54:10 +0300

    Adjust stay.service.local

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -1,3 +1,16 @@
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
 export function StayDetails() {
+
+  const {stayId} = useParams()
+  const stay = useSelector(storeState => storeState.stayModule.stay)
+
+  useEffect(() => {
+    loadCar(stayId)
+  }, [stayId])
+
   return <h1>Hello from StayDetails</h1>
 }
--- src/services/stay/stay.service.local.js
+++ src/services/stay/stay.service.local.js
@@ -12,7 +12,6 @@ export const stayService = {
     getById,
     save,
     remove,
-    // addStayMsg
 }
 window.cs = stayService
 
@@ -55,33 +54,15 @@ async function save(stay) {
     return savedStay
 }
 
-// async function addStayMsg(stayId, txt) {
-//     // Later, this is all done by the backend
-//     const stay = await getById(stayId)
-
-//     const msg = {
-//         id: makeId(),
-//         by: userService.getLoggedinUser(),
-//         txt
-//     }
-//     stay.msgs.push(msg)
-//     await storageService.put(STORAGE_KEY, stay)
-
-//     return msg
-// }
-
 function _createStays() {
     gStays = loadFromStorage(STORAGE_KEY)
     if (gStays && gStays.length) return
 
     // If no stays in storage - generate demo data
     gStays = []
-    gStays.push(_createStay('puki'))
-    gStays.push(_createStay('muki'))
-    gStays.push(_createStay('bobo'))
-    gStays.push(_createStay('popo'))
-    gStays.push(_createStay('lala'))
-    gStays.push(_createStay('dolev'))
+    gStays.push(_createStay('House 1', 'Apartment', 500))
+    gStays.push(_createStay('House 2', 'Villa', 1000))
+    gStays.push(_createStay('House 3', 'Penthouse', 1200))
     _saveStaysToStorage()
 }
 
@@ -89,49 +70,42 @@ function _saveStaysToStorage() {
     saveToStorage(STORAGE_KEY, gStays)
 }
 
-function _createStay(name) {
+function _createStay(name, type, price) {
     return {
         _id: makeId(),
         name,
+        type,
+        imgUrls: ['https://e26e9b.jpg', 'otherImg.jpg'],
+        price,
+        summary: 'Fantastic duplex apartment...',
+        capacity: 8,
+        amenities: ['TV', 'Wifi', 'Kitchen', 'Smoking allowed', 'Pets allowed', 'Cooking basics'],
+        labels: ['Top of the world', 'Trending', 'Play', 'Tropical'],
+        host: {
+            _id: makeId(),
+            fullname: 'Davit Pok',
+            imgUrl: 'https://a0.muscache.com/im/pictures/fab79f25-2e10-4f0f-9711-663cb69dc7d8.jpg?aki_policy=profile_small',
+        },
+        loc: {
+            country: 'Portugal',
+            countryCode: 'PT',
+            city: 'Lisbon',
+            address: '17 Kombo st',
+            lat: -8.61308,
+            lng: 41.1413,
+        },
+        reviews: [
+            {
+                id: makeId(),
+                txt: 'Very helpful hosts. Cooked traditional...',
+                rate: 4,
+                by: {
+                    _id: makeId(),
+                    fullname: 'user2',
+                    imgUrl: '/img/img2.jpg',
+                },
+            },
+        ],
+        likedByUsers: ['mini-user'],
     };
 }
-
-// function _createStay() {
-//     return {
-//         _id: utilService.makeId(),
-//         name: 'Ribeira Charming Duplex',
-//         type: 'House',
-//         imgUrls: ['https://e26e9b.jpg', 'otherImg.jpg'],
-//         price: 80.0,
-//         summary: 'Fantastic duplex apartment...',
-//         capacity: 8,
-//         amenities: ['TV', 'Wifi', 'Kitchen', 'Smoking allowed', 'Pets allowed', 'Cooking basics'],
-//         labels: ['Top of the world', 'Trending', 'Play', 'Tropical'],
-//         host: {
-//             _id: utilService.makeId(),
-//             fullname: 'Davit Pok',
-//             imgUrl: 'https://a0.muscache.com/im/pictures/fab79f25-2e10-4f0f-9711-663cb69dc7d8.jpg?aki_policy=profile_small',
-//         },
-//         loc: {
-//             country: 'Portugal',
-//             countryCode: 'PT',
-//             city: 'Lisbon',
-//             address: '17 Kombo st',
-//             lat: -8.61308,
-//             lng: 41.1413,
-//         },
-//         reviews: [
-//             {
-//                 id: utilService.makeId(),
-//                 txt: 'Very helpful hosts. Cooked traditional...',
-//                 rate: 4,
-//                 by: {
-//                     _id: utilService.makeId(),
-//                     fullname: 'user2',
-//                     imgUrl: '/img/img2.jpg',
-//                 },
-//             },
-//         ],
-//         likedByUsers: ['mini-user'],
-//     };
-// }
