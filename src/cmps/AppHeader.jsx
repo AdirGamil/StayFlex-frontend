08d03eb4166bef46b8201d297ebdc329a098451a
Gal Shimer <galshimer2@gmail.com>
2024-07-20 13:26:20 +0300

    Add menu button

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,16 +1,42 @@
+// import { Link, NavLink } from 'react-router-dom'
+// import logo from '../assets/img/airbnb-icon.svg'
+
+// export function AppHeader() {
+//   return (
+//     <header className="app-header">
+//       <nav className="nav-container">
+//         <NavLink to="/" className="logo">
+//           {/* <img
+//             className="logo-header"
+//             src="https://res.cloudinary.com/dhweqnxgd/image/upload/v1721279278/StayFlex%20App/wzb3ksekaeh5zogdfmqr.png"
+//             alt="logo"
+//           /> */}
+//           <img className="app-logo" src={logo} alt="logo" />
+//           <h1>StayFlex</h1>
+//         </NavLink>
+//         <div className="links-header">
+//           <NavLink to="stay">Stays</NavLink>
+//           <NavLink to="experiences">Experiences</NavLink>
+//         </div>
+
+//         <NavLink to="login" className="login-link">
+//           Login
+//         </NavLink>
+//         <button> menu </button>
+//       </nav>
+//     </header>
+//   )
+// }
+import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../assets/img/airbnb-icon.svg'
+import MenuButton from './MenuButton' // Import the MenuButton component
 
 export function AppHeader() {
   return (
     <header className="app-header">
       <nav className="nav-container">
         <NavLink to="/" className="logo">
-          {/* <img
-            className="logo-header"
-            src="https://res.cloudinary.com/dhweqnxgd/image/upload/v1721279278/StayFlex%20App/wzb3ksekaeh5zogdfmqr.png"
-            alt="logo"
-          /> */}
           <img className="app-logo" src={logo} alt="logo" />
           <h1>StayFlex</h1>
         </NavLink>
@@ -18,10 +44,12 @@ export function AppHeader() {
           <NavLink to="stay">Stays</NavLink>
           <NavLink to="experiences">Experiences</NavLink>
         </div>
-
-        <NavLink to="login" className="login-link">
-          Login
-        </NavLink>
+        <div className="user-actions">
+          {/* <NavLink to="login" className="login-link">
+            Login
+          </NavLink> */}
+          <MenuButton />
+        </div>
       </nav>
     </header>
   )
---
+++ src/cmps/MenuButton.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useRef, useEffect } from 'react'
+const MenuButton = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const dropdownRef = useRef(null)
+
+    const toggleDropdown = () => setIsOpen(!isOpen)
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsOpen(false)
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => document.removeEventListener('mousedown', handleClickOutside)
+    }, [])
+
+    // return (
+    //     <div className="menu-button-container" ref={dropdownRef}>
+    //         <button className="menu-button" onClick={toggleDropdown}>
+    //             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" className="menu-icon">
+    //                 <g fill="none" fillRule="nonzero">
+    //                     <path d="m2 16h28"></path>
+    //                     <path d="m2 24h28"></path>
+    //                     <path d="m2 8h28"></path>
+    //                 </g>
+    //             </svg>
+    //             <div className="divider"></div>
+    //             <div className="profile-icon-wrapper">
+    //                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" className="profile-icon">
+    //                     <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
+    //                 </svg>
+    //             </div>
+    //         </button>
+    //         {isOpen && (
+    //             <div className="dropdown-menu">
+    //                 <a href="#">Sign up</a>
+    //                 <a href="#">Log in</a>
+    //                 <a href="#">Gift cards</a>
+    //                 <a href="#">Airbnb your home</a>
+    //                 <a href="#">Help Center</a>
+    //             </div>
+    //         )}
+    //     </div>
+    // )
+    return (
+        <div className="menu-button-container" ref={dropdownRef}>
+            <button className="menu-button" onClick={toggleDropdown}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" className="menu-icon">
+                    <g fill='$clr6' fillRule="nonzero"> {/* Use $clr7 variable */}
+                        <path d="m2 16h28"></path>
+                        <path d="m2 24h28"></path>
+                        <path d="m2 8h28"></path>
+                    </g>
+                </svg>
+                <div className="divider"></div>
+                <div className="profile-icon-wrapper">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" className="profile-icon">
+                        <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
+                    </svg>
+                </div>
+            </button>
+            {isOpen && (
+                <div className="dropdown-menu">
+                    <a href="#">Sign up</a>
+                    <a href="#">Log in</a>
+                    <a href="#">Gift cards</a>
+                    <a href="#">Airbnb your home</a>
+                    <a href="#">Help Center</a>
+                </div>
+            )}
+        </div>
+    );
+
+}
+
+export default MenuButton
