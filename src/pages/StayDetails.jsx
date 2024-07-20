aa5ec5436cc929f313149b29397a7a007cc990b8
Denis Libin <dnslibin4@gmail.com>
2024-07-20 13:14:27 +0300

    Split StayDetails reservation section into separate Cmp

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -6,55 +6,19 @@ import "react-datepicker/dist/react-datepicker.css"
 import { loadStay } from '../store/actions/stay.actions'
 
 import { StayGallery } from '../cmps/StayDetailsCmps/StayGallery'
+import { StayReservation } from '../cmps/StayDetailsCmps/StayReservation'
 
 const starIcon = 'https://res.cloudinary.com/dhweqnxgd/image/upload/v1721294785/star_us9ozb.png'
 
 export function StayDetails() {
   const { stayId } = useParams()
   const stay = useSelector(storeState => storeState.stayModule.stay)
-  const [startDate, setStartDate] = useState(null)
-  const [endDate, setEndDate] = useState(null)
-  const [guests, setGuests] = useState({
-    adults: 1,
-    children: 0,
-    infants: 0,
-    pets: 0
-  })
-  const [isGuestsDropdownOpen, setIsGuestsDropdownOpen] = useState(false)
-  const [isRegionDropdownOpen, setIsRegionDropdownOpen] = useState(false)
+
 
   useEffect(() => {
     loadStay(stayId)
   }, [stayId])
 
-  function handleStartDateChange(date) {
-    setStartDate(date)
-  }
-
-  function handleEndDateChange(date) {
-    setEndDate(date)
-  }
-
-  function toggleGuestsDropdown() {
-    setIsGuestsDropdownOpen(!isGuestsDropdownOpen);
-  }
-
-  function handleGuestsChange(type, value) {
-    const newGuests = { ...guests, [type]: Math.max(0, value) };
-    setGuests(newGuests);
-    setFilterToEdit({ ...filterToEdit, guests: newGuests });
-  }
-
-  const calculateNumberOfNights = () => {
-    if (!startDate || !endDate) return 0;
-    const timeDiff = endDate.getTime() - startDate.getTime();
-    return Math.ceil(timeDiff / (1000 * 3600 * 24));
-  };
-
-  const numberOfNights = calculateNumberOfNights();
-  const subtotal = stay ? stay.price * numberOfNights : 0;
-  const taxes = subtotal * 0.17; // 17% of the subtotal
-  const totalPrice = subtotal + taxes;
 
   if (!stay) return <div>Loading...</div>
 
@@ -87,9 +51,9 @@ export function StayDetails() {
             <h2>{stay.type} in {stay.loc.city}, {stay.loc.country}</h2>
 
             <ul className="info-list">
-              <li>{stay.capacity} guests</li>
-              <li>4 bedrooms</li>
-              <li>4 beds</li>
+              <li>{stay.capacity} guests</li>  ·
+              <li>4 bedrooms</li>  ·
+              <li>4 beds</li>  ·
               <li>2 baths</li>
             </ul>
 
@@ -100,7 +64,7 @@ export function StayDetails() {
                   src={starIcon}
                   alt="star icon"
                 />
-                {stay.reviews && stay.reviews.length > 0 ? stay.reviews[0].rate : 'N/A'}.0
+                {stay.reviews && stay.reviews.length > 0 ? stay.reviews[0].rate.toFixed(1) : 'N/A'}  ·
               </div>
               <a href='' className="stay-reviews">{stay.reviews ? stay.reviews.length : 0} reviews</a>
             </div>
@@ -121,103 +85,7 @@ export function StayDetails() {
           </div>
         </div>
 
-        {/* //RESERVATION SECTION */}
-
-        <div className="reservation">
-          <div className="reservation-selectors">
-            <div className="reservation-price">
-              <h2>${stay.price}&nbsp;</h2>
-              <span>night</span>
-            </div>
-          </div>
-
-          <div className="date-picker-container">
-            <div className="check-in">
-              {/* <label>Check in</label> */}
-              <DatePicker
-                selected={startDate}
-                onChange={handleStartDateChange}
-                selectsStart
-                startDate={startDate}
-                endDate={endDate}
-                monthsShown={2}
-                dateFormat="dd/MM/yyyy"
-                placeholderText="CHECK-IN"
-              />
-            </div>
-            <div className="check-out">
-              {/* <label>Check out</label> */}
-              <DatePicker
-                selected={endDate}
-                onChange={handleEndDateChange}
-                selectsEnd
-                startDate={startDate}
-                endDate={endDate}
-                minDate={startDate}
-                monthsShown={2}
-                dateFormat="dd/MM/yyyy"
-                placeholderText="CHECKOUT"
-              />
-            </div>
-          </div>
-
-          <div className="guests-selector">
-            <button className="guests-button" onClick={toggleGuestsDropdown}>
-              {/* <span>Who</span> */}
-              <span>
-                {guests.adults + guests.children} guests
-                {guests.infants > 0 && `, ${guests.infants} infant${guests.infants > 1 ? 's' : ''}`}
-                {guests.pets > 0 && `, ${guests.pets} pet${guests.pets > 1 ? 's' : ''}`}
-              </span>
-            </button>
-            <div className={`guests-dropdown ${isGuestsDropdownOpen ? 'active' : ''}`}>
-              {[
-                { type: 'adults', label: 'Adults', subLabel: 'Ages 13 or above' },
-                { type: 'children', label: 'Children', subLabel: 'Ages 2-12' },
-                { type: 'infants', label: 'Infants', subLabel: 'Under 2' },
-                { type: 'pets', label: 'Pets', subLabel: 'Bringing a service animal?' },
-              ].map(({ type, label, subLabel }) => (
-                <div key={type}>
-                  <div className="guest-type">
-                    <label>{label}</label>
-                    <span>{subLabel}</span>
-                  </div>
-                  <div className="guest-counter">
-                    <button
-                      onClick={() => handleGuestsChange(type, guests[type] - 1)}
-                      disabled={guests[type] === 0 || (type === 'adults' && guests[type] === 1)}
-                    >
-                      -
-                    </button>
-                    <span>{guests[type]}</span>
-                    <button onClick={() => handleGuestsChange(type, guests[type] + 1)}>
-                      +
-                    </button>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          <button className="reserve-btn">Reserve</button>
-          <p className="charged-p">You won't be charged yet</p>
-
-          <div className="price-details">
-            <div className="price-item">
-              <span>${stay.price} x {numberOfNights} nights</span>
-              <span>${stay.price * numberOfNights}</span>
-            </div>
-            <div className="price-item">
-              <span>Taxes</span>
-              <span>${taxes.toFixed(2)}</span>
-            </div>
-            <div className="price-total">
-              <span>Total</span>
-              <span>${totalPrice.toFixed(2)}</span>
-            </div>
-          </div>
-        </div>
-
+        <StayReservation stay={stay}/>
       </section>
     </section>
   )
