bca1592a6124f6e916db5545badde8616b3c74c4
Denis Libin <dnslibin4@gmail.com>
2024-07-20 10:27:52 +0300

    Clean up at StayDetails

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -1,107 +1,3 @@
-// {/* <Link to="/">Back to list</Link> */ }
-// const starIcon =
-//   'https://res.cloudinary.com/dhweqnxgd/image/upload/v1721294785/star_us9ozb.png'
-
-
-
-// import { useEffect, useState } from 'react'
-// import { useParams, Link } from 'react-router-dom'
-// import { useSelector } from 'react-redux'
-
-
-// import { loadStay } from '../store/actions/stay.actions'
-
-
-// export function StayDetails() {
-
-//   const { stayId } = useParams()
-//   const stay = useSelector(storeState => storeState.stayModule.stay)
-
-//   useEffect(() => {
-//     loadStay(stayId)
-//   }, [stayId])
-
-//   if (!stay) return <div>Loading...</div>
-
-//   return (
-//     <section className="stay-details main-layout">
-
-//       <section className="stay-header">
-
-//         <h1 className="stay-title">
-//           {stay.name} 4 bedroom {stay.type}
-//         </h1>
-
-//         <div className="share-save">
-//           <a href="">Share</a>
-//           <a href="">Save</a>
-//         </div>
-
-//       </section>
-
-//       <div className="stay-gallery">
-//         <img src={stay.imgUrls} alt="" />
-//       </div>
-
-//       <section className="info-reservation">
-
-//         <div className="stay-info">
-
-//           <h2>{stay.type} in {stay.loc.city}, {stay.loc.country}</h2>
-
-//           <ul className="info-list">
-//             <li>{stay.capacity} guests</li>
-//             <li>4 bedrooms</li>
-//             <li>4 beds</li>
-//             <li>2 baths</li>
-//           </ul>
-
-//           <div className="rating-reviews">
-
-//             <div className="stay-rating">
-//               <img
-//                 className="stay-rating-star-icon"
-//                 src={starIcon}
-//                 alt="star icon"
-//               />
-//               {stay.reviews[0].rate}.0
-//             </div>
-//             <a href='' className="stay-reviews">5 reviews</a>
-//           </div>
-//           <div className="stay-host-details">
-
-//             <div className="host">
-//               {/* <img src={stay.host.imgUrl} alt="" /> */}
-//               <div>Hosted by {stay.host}</div>
-//             </div>
-//           </div>
-
-//           <div className="stay-description">
-//             <p>
-//               {stay.summary}
-//             </p>
-//           </div>
-//         </div>
-
-
-//         <div className="reservation">
-//           <div className="reservation-selectors">
-//             <div className="reservation-price">
-//               <h2>
-//                 ${stay.price}
-//                 &nbsp;
-//               </h2>
-//               <span>night</span>
-//             </div>
-//           </div>
-//           <button className="reserve-btn">Reserve</button>
-//           <p className="charged-p">You won't be charged yet.</p>
-//         </div>
-//       </section>
-//     </section>
-//   )
-// }
-
 import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
@@ -147,6 +43,16 @@ export function StayDetails() {
     setFilterToEdit({ ...filterToEdit, guests: newGuests });
   }
 
+  const calculateNumberOfNights = () => {
+    if (!startDate || !endDate) return 0;
+    const timeDiff = endDate.getTime() - startDate.getTime();
+    return Math.ceil(timeDiff / (1000 * 3600 * 24));
+  };
+
+  const numberOfNights = calculateNumberOfNights();
+  const subtotal = stay ? stay.price * numberOfNights : 0;
+  const taxes = subtotal * 0.17; // 17% of the subtotal
+  const totalPrice = subtotal + taxes;
 
   if (!stay) return <div>Loading...</div>
 
@@ -218,7 +124,7 @@ export function StayDetails() {
 
           <div className="date-picker-container">
             <div className="check-in">
-              <label></label>
+              {/* <label>Check in</label> */}
               <DatePicker
                 selected={startDate}
                 onChange={handleStartDateChange}
@@ -227,11 +133,11 @@ export function StayDetails() {
                 endDate={endDate}
                 monthsShown={2}
                 dateFormat="dd/MM/yyyy"
-                placeholderText="Check in"
+                placeholderText="CHECK-IN"
               />
             </div>
             <div className="check-out">
-              <label></label>
+              {/* <label>Check out</label> */}
               <DatePicker
                 selected={endDate}
                 onChange={handleEndDateChange}
@@ -241,52 +147,68 @@ export function StayDetails() {
                 minDate={startDate}
                 monthsShown={2}
                 dateFormat="dd/MM/yyyy"
-                placeholderText="Check out"
+                placeholderText="CHECKOUT"
               />
             </div>
           </div>
 
           <div className="guests-selector">
-                <button className="guests-button" onClick={toggleGuestsDropdown}>
-                    <span>
-                    {/* <label>GUESTS</label> */}
-                        {guests.adults + guests.children} guests
-                        {guests.infants > 0 && `, ${guests.infants} infant${guests.infants > 1 ? 's' : ''}`}
-                        {guests.pets > 0 && `, ${guests.pets} pet${guests.pets > 1 ? 's' : ''}`}
-                    </span>
-                </button>
-                <div className={`guests-dropdown ${isGuestsDropdownOpen ? 'active' : ''}`}>
-                    {[
-                        { type: 'adults', label: 'Adults', subLabel: 'Ages 13 or above' },
-                        { type: 'children', label: 'Children', subLabel: 'Ages 2-12' },
-                        { type: 'infants', label: 'Infants', subLabel: 'Under 2' },
-                        { type: 'pets', label: 'Pets', subLabel: 'Bringing a service animal?' },
-                    ].map(({ type, label, subLabel }) => (
-                        <div key={type}>
-                            <div className="guest-type">
-                                <label>{label}</label>
-                                <span>{subLabel}</span>
-                            </div>
-                            <div className="guest-counter">
-                                <button
-                                    onClick={() => handleGuestsChange(type, guests[type] - 1)}
-                                    disabled={guests[type] === 0 || (type === 'adults' && guests[type] === 1)}
-                                >
-                                    -
-                                </button>
-                                <span>{guests[type]}</span>
-                                <button onClick={() => handleGuestsChange(type, guests[type] + 1)}>
-                                    +
-                                </button>
-                            </div>
-                        </div>
-                    ))}
+            <button className="guests-button" onClick={toggleGuestsDropdown}>
+              {/* <span>Who</span> */}
+              <span>
+                {guests.adults + guests.children} guests
+                {guests.infants > 0 && `, ${guests.infants} infant${guests.infants > 1 ? 's' : ''}`}
+                {guests.pets > 0 && `, ${guests.pets} pet${guests.pets > 1 ? 's' : ''}`}
+              </span>
+            </button>
+            <div className={`guests-dropdown ${isGuestsDropdownOpen ? 'active' : ''}`}>
+              {[
+                { type: 'adults', label: 'Adults', subLabel: 'Ages 13 or above' },
+                { type: 'children', label: 'Children', subLabel: 'Ages 2-12' },
+                { type: 'infants', label: 'Infants', subLabel: 'Under 2' },
+                { type: 'pets', label: 'Pets', subLabel: 'Bringing a service animal?' },
+              ].map(({ type, label, subLabel }) => (
+                <div key={type}>
+                  <div className="guest-type">
+                    <label>{label}</label>
+                    <span>{subLabel}</span>
+                  </div>
+                  <div className="guest-counter">
+                    <button
+                      onClick={() => handleGuestsChange(type, guests[type] - 1)}
+                      disabled={guests[type] === 0 || (type === 'adults' && guests[type] === 1)}
+                    >
+                      -
+                    </button>
+                    <span>{guests[type]}</span>
+                    <button onClick={() => handleGuestsChange(type, guests[type] + 1)}>
+                      +
+                    </button>
+                  </div>
                 </div>
+              ))}
             </div>
+          </div>
 
           <button className="reserve-btn">Reserve</button>
-          <p className="charged-p">You won't be charged yet.</p>
+          <p className="charged-p">You won't be charged yet</p>
+
+          <div className="price-details">
+            <div className="price-item">
+              <span>${stay.price} x {numberOfNights} nights</span>
+              <span>${stay.price * numberOfNights}</span>
+            </div>
+            <div className="price-item">
+              <span>Taxes</span>
+              <span>${taxes.toFixed(2)}</span>
+            </div>
+            <div className="price-total">
+              <span>Total</span>
+              <span>${totalPrice.toFixed(2)}</span>
+            </div>
+          </div>
         </div>
+
       </section>
     </section>
   )
