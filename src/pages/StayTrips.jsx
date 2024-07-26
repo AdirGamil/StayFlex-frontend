31b6a30fea512700b719d8de920c3864382d1fb8
Gal Shimer <galshimer2@gmail.com>
2024-07-26 10:33:47 +0300

    adding css to trips

--- src/pages/StayTrips.jsx
+++ src/pages/StayTrips.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { orderService } from '../services/order'
+import { NavLink } from 'react-router-dom'
+
 
 export function StayTrips() {
   const [orders, setOrders] = useState([])
@@ -15,16 +17,21 @@ export function StayTrips() {
 
   return (
     <section className="stay-trip-container">
+      <h1>Trips</h1>
       {orders.map((order) => (
         <article key={order._id} className="stay-trip">
           <img src={order.stay.imgUrls[0]} alt="" className="trip-img" />
-          <div className="trip-city">{order.stay.loc.city}</div>
-          <div className="trip-title">Hosted by {order.hostId.fullname}</div>
-          <div className="trip-date">Start Date: {order.startDate}</div>
-          <div className="trip-date">End Date: {order.endDate}</div>
-          <div className="trip-status">{order.status}</div>
+          <div>
+            <div className="trip-city">{order.stay.loc.city}</div>
+            <div className="trip-title">Hosted by {order.hostId.fullname}</div>
+            <div className="trip-date">Start Date: {order.startDate}</div>
+            <div className="trip-date">End Date: {order.endDate}</div>
+            <div className="trip-status">{order.status}</div>
+          </div>
         </article>
       ))}
+      <div className="trip-not"> Cant find your reservation here? <span> Lets find one </span>
+      </div>
     </section>
   )
 }
