753df471d8f0b726bd9944a6f9da85d1b83ba7dc
Gal Shimer <galshimer2@gmail.com>
2024-07-28 09:30:54 +0300

    fixing API key to map

--- src/cmps/StayDetailsCmps/Map.jsx
+++ src/cmps/StayDetailsCmps/Map.jsx
@@ -1,88 +1,36 @@
 import React, { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
-// import logoUrl from '../assets/img/toys-favicon.png'
 
 function Marker() {
     return (
         <div className="branch-img">
-            {/* <img src={} /> */}
-            <p>❤️</p>
+            {/* <h1>📌</h1> */}
         </div>
     )
 }
 
-const API_KEY = import.meta.env.GOOGLE_MAP_API || 'AIzaSyD051H7dzgi09CB_LuUyUnF40rutHzSGd4'
+const API_KEY = import.meta.env.GOOGLE_MAP_API || 'AIzaSyBqsrRQVeaOGYAoaXez4Kxa3cSKvektDiw'
 
 export function Map({ stay }) {
     const [coordinates, setCoordinates] = useState({
         lat: stay.loc.lat,
         lng: stay.loc.lng
-    });
+    })
     const [zoom, setZoom] = useState(8)
-
-    // function handleClick({ lat, lng }) {
-    //     setCoordinates({ lat, lng })
-    // }
-
-    // const branches = [
-    //     {
-    //         city: 'Haifa',
-    //         id: 101,
-    //         position: {
-    //             lat: 32.820789,
-    //             lng: 34.963488,
-    //         },
-    //     },
-    //     {
-    //         city: 'Tel Aviv',
-    //         id: 102,
-    //         position: {
-    //             lat: 32.071035,
-    //             lng: 34.779118,
-    //         },
-    //     },
-    //     {
-    //         city: 'Jerusalem',
-    //         id: 103,
-    //         position: {
-    //             lat: 31.773362,
-    //             lng: 35.221193,
-    //         },
-    //     },
-    // ]
 
     return (
         <div>
-            {/* {branches.map(branch => {
-                return (
-                    <button
-                        key={branch.city}
-                        onClick={() => {
-                            // setCoordinates(branch.position)
-                            setZoom(12)
-                        }}
-                    >
-                        {branch.city}
-                    </button>
-                )
-            })} */}
             {/* // Important! Always set the container height explicitly */}
             <div className="map" style={{ height: '60vh', width: '100%' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: API_KEY }}
                     center={coordinates}
                     zoom={zoom}
-                // onClick={handleClick}
                 >
-                    {/* {branches.map(branch => {
-                        return (
-                            <Marker
-                                lat={branch.position.lat}
-                                lng={branch.position.lng}
-                                key={branch.id}
-                            />
-                        )
-                    })} */}
+                    <Marker
+                        lat={coordinates.lat}
+                        lng={coordinates.lng}
+                    />
                 </GoogleMapReact>
             </div>
         </div>
