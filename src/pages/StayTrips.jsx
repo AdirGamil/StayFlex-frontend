e7558245b75c72c7f849f9157044a990bf0f7994
AdirGamil <2000adir2000@gmail.com>
2024-07-28 21:00:03 +0300

    StayTrip update

--- src/pages/StayTrips.jsx
+++ src/pages/StayTrips.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { orderService } from '../services/order'
 import { NavLink } from 'react-router-dom'
 
-
 export function StayTrips() {
   const [orders, setOrders] = useState([])
 
@@ -22,8 +21,8 @@ export function StayTrips() {
         <article key={order._id} className="stay-trip">
           {console.log(order)}
           <img src={order.stay.imgUrls[0]} alt="" className="trip-img" />
-          <div>
-            <div className="trip-city">{order.stay.loc.city}</div>
+          <div className="trip-info">
+            <div className="trip-city bold">{order.stay.loc.city}</div>
             <div className="trip-title">Hosted by {order.hostId.fullname}</div>
             <div className="trip-date">Start Date: {order.startDate}</div>
             <div className="trip-date">End Date: {order.endDate}</div>
@@ -31,7 +30,9 @@ export function StayTrips() {
           </div>
         </article>
       ))}
-      <div className="trip-not"> Cant find your reservation here? <span> Lets find one </span>
+      <div className="trip-not">
+        {' '}
+        Cant find your reservation here? <span> Lets find one </span>
       </div>
     </section>
   )
