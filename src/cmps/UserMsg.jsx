9ee0bed33ebb3951c029c5af943c481dca2be1d7
AdirGamil <2000adir2000@gmail.com>
2024-07-30 18:52:29 +0300

    update usermsg css

--- src/cmps/UserMsg.jsx
+++ src/cmps/UserMsg.jsx
@@ -1,24 +1,26 @@
+import { useEffect, useRef, useState } from 'react'
 import { eventBus, showSuccessMsg } from '../services/event-bus.service'
-import { useState, useEffect, useRef } from 'react'
 import {
   socketService,
   SOCKET_EVENT_ORDER_ADDED,
 } from '../services/socket.service'
+import Alert from '@mui/material/Alert'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
 
 export function UserMsg() {
   const [msg, setMsg] = useState(null)
   const timeoutIdRef = useRef()
 
-	useEffect(() => {
-		const unsubscribe = eventBus.on('show-msg', msg => {
-			setMsg(msg)
-			if (timeoutIdRef.current) {
-				timeoutIdRef.current = null
-				clearTimeout(timeoutIdRef.current)
-			}
-			timeoutIdRef.current = setTimeout(closeMsg, 3000)
-		})
-
+  useEffect(() => {
+    const unsubscribe = eventBus.on('show-msg', (msg) => {
+      setMsg(msg)
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current)
+        timeoutIdRef.current = null
+      }
+      timeoutIdRef.current = setTimeout(closeMsg, 12000000)
+    })
 
     socketService.on(SOCKET_EVENT_ORDER_ADDED, (order) => {
       showSuccessMsg(`New Order: ${order._id}`)
@@ -38,10 +40,24 @@ export function UserMsg() {
     return msg ? 'visible' : ''
   }
 
+  if (!msg) return <span></span>
   return (
     <section className={`user-msg ${msg?.type} ${msgClass()}`}>
-      <button onClick={closeMsg}>x</button>
-      {msg?.txt}
+      <Alert
+        severity={msg.type}
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={closeMsg}
+          >
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+        }
+      >
+        {msg.txt}
+      </Alert>
     </section>
   )
 }
